7d85a3dfabd69c9354989ac8a28f84a7ee04084a
Lolesterno <jose_nico2000@hotmail.com>
2023-08-31 08:40:57 -0500

    End Front Form

--- resources/js/Pages/Parts/Index.jsx
+++ resources/js/Pages/Parts/Index.jsx
@@ -2,9 +2,13 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { Head } from '@inertiajs/react'
 import { Input, Select, SelectItem, CheckboxGroup, Checkbox } from '@nextui-org/react'
 
-export default function Parts ({ auth, modifiableparts, soParts, aditionals }) {
+export default function Parts ({ auth, soParts, aditionals }) {
   const handleModificablesParts = (e) => {
-    console.log(e.target)
+    console.log(e.target.value)
+  }
+
+  const handleSo = (e) => {
+    console.log(e.target.value)
   }
   return (
     <AuthenticatedLayout
@@ -23,21 +27,48 @@ export default function Parts ({ auth, modifiableparts, soParts, aditionals }) {
                 </h2>
                 <form>
                   <div className='flex gap-2'>
-                    {modifiableparts.map(({ id, product, description, usdPrice }) => (
-                      <Input key={id} type='number' label={`Cantidad de ${product}`} radius='sm' labelPlacement='inside' onChange={handleModificablesParts} />
-                    ))}
+                    <Input
+                      type='number' label='Numero de Nucleos de CPU' radius='sm' labelPlacement='inside' description='Procesador' defaultValue={1} onChange={handleModificablesParts} endContent={
+                        <div className='pointer-events-none flex items-center'>
+                          <span className='text-default-400 text-small'>Nucleos</span>
+                        </div>
+                      }
+                    />
+                    <Input
+                      type='number' label='Memoria RAM' radius='sm' labelPlacement='inside' description='Memoria RAM' defaultValue={1} onChange={handleModificablesParts} endContent={
+                        <div className='pointer-events-none flex items-center'>
+                          <span className='text-default-400 text-small'>GB</span>
+                        </div>
+                      }
+                    />
+                  </div>
+                  <div className='flex gap-2'>
+                    <Input
+                      type='number' label='Almacenamiento' radius='sm' labelPlacement='inside' description='Almacenamiento' defaultValue={10} onChange={handleModificablesParts} endContent={
+                        <div className='pointer-events-none flex items-center'>
+                          <span className='text-default-400 text-small'>GB</span>
+                        </div>
+                      }
+                    />
+                    <Input
+                      type='number' label='Ancho de Banda' radius='sm' labelPlacement='inside' description='Ancho de Banda' defaultValue={1} onChange={handleModificablesParts} endContent={
+                        <div className='pointer-events-none flex items-center'>
+                          <span className='text-default-400 text-small'>Mb/s</span>
+                        </div>
+                      }
+                    />
                   </div>
                   <div className='flex my-2 gap-2'>
-                    <Select label='Sistema Operativo S.O' className='max-w-xs text-black'>
+                    <Select label='Sistema Operativo S.O' className='max-w-xs text-black' onChange={handleSo}>
                       {soParts.map(({ id, product, description, usdPrice }) => (
-                        <SelectItem key={id} value={usdPrice}> {product} </SelectItem>
+                        <SelectItem key={id} value={usdPrice} textValue={product}> {product} </SelectItem>
                       ))}
                     </Select>
                   </div>
                   <div className='flex gap-2'>
                     <CheckboxGroup label='Adicionales'>
-                      {aditionals.map(({ id, product, description, usdPrice }) => (
-                        <Checkbox key={id} value={usdPrice}> {product} </Checkbox>
+                      {aditionals.map(({ id, product }) => (
+                        <Checkbox key={id} value={id} onChange={handleSo}> {product} </Checkbox>
                       ))}
                     </CheckboxGroup>
                   </div>
