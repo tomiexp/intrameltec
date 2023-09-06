f7c3573c7e398a57c7d97944b199c7975ed1b9a8
Lolesterno <jose_nico2000@hotmail.com>
2023-09-06 16:52:18 -0500

    Update - Delete Directors Policy - Changes to Gate

--- app/Http/Controllers/DirectorsController.php
+++ app/Http/Controllers/DirectorsController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 
 class DirectorsController extends Controller
 {
     public function __invoke()
     {
-        $this->authorize('viewAny', User::class);
+        if(!Gate::allows('kpisDirectorAccess')) {
+            abort(403);
+        }
         return Inertia::render('Directors/Index');
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
-
 use App\Models\User;
 use App\Policies\RolPolicy;
 use App\Policies\UserPolicy;
-use App\Policies\DirectorPolicy;
 use Spatie\Permission\Models\Role;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -22,7 +19,6 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Role::class => RolPolicy::class,
         User::class => UserPolicy::class,
-        User::class => DirectorPolicy::class,
     ];
 
     /**
@@ -33,10 +29,16 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         // Grant "Super Administrador" role all permissions 
-        Gate::before(function ($user, $ability) {
+        Gate::before(function (User $user, $ability) {
             if($user->hasRole('Super Administrador')) {
                 return true;
             }
+        });
+
+        // Gate Define to Index Kpi's
+
+        Gate::define('kpisDirectorAccess', function(User $user) {
+            return $user->hasRole('Director') || $user->hasRole('Administrador');
         });
     }
 }
