3057ce3f48cdd395f7877b761c418b4a7466246c
Lolesterno <jose_nico2000@hotmail.com>
2023-09-06 17:05:59 -0500

    New Commit

--- app/Http/Controllers/DirectorsController.php
+++ app/Http/Controllers/DirectorsController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class DirectorsController extends Controller
 {
     public function __invoke()
     {
+        if(!Gate::allows('kpisDirectorAccess')) {
+            abort(403);
+        }
         return Inertia::render('Directors/Index');
     }
 }
