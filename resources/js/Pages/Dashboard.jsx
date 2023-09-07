23c1f60582fa78a3db6c3d20e9b9bc1c0a40fa6d
Lolesterno <jose_nico2000@hotmail.com>
2023-09-07 11:50:10 -0500

    Implements the Laravel Events to RealTime and ADD laravel packages

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -5,6 +5,9 @@ import { TrmGraph } from '@/Components/Trm'
 
 export default function Dashboard ({ auth }) {
   const { valores, loading, trmInCop } = useTrm()
+  // eslint-disable-next-line no-undef
+  Echo.private('events')
+    .listen('RealTimeMessage', (e) => console.log('RealTimeMessage: ' + e.message))
   return (
     <AuthenticatedLayout
       user={auth.user}
