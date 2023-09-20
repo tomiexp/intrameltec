05ca917bbf7576420c0fcd9ae53f46962eaa8171
Meltec IT Ubuntu <jcuadros@meltec.com.co>
2023-09-20 18:05:50 -0500

    Change Title quote

--- resources/js/Pages/Commercial/Quoter.jsx
+++ resources/js/Pages/Commercial/Quoter.jsx
@@ -14,7 +14,7 @@ export default function Quoter ({ auth, unreadNotifications }) {
         <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
           <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg'>
             <h2 className='text-center font-bold text-xl mt-3'>
-              Informes de KPI Meltec
+              Cotizador
             </h2>
             <div className='p-6'>
               <iframe title='Report Section' width='100%' height='612' style={{ height: '75vh' }} src='https://app.powerbi.com/view?r=eyJrIjoiZDVkZTNlZTQtNDcxNi00NWY5LWIyNmEtZjU4OTlmYmI1NjcxIiwidCI6ImQzOTZjNGY4LTMyOTgtNDg5ZS04YmQ0LTFkZjZiMmE0NzE4NCJ9' allowFullScreen />
